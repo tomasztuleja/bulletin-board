a457b890c61c5ca7757d0e9e726aea4c257e5a2c
tomalek <tomalek0@o2.pl>
2017-06-14 22:14:08 +0200

    Project in creation

---
+++ src/main/java/com/ttuleja/Main.java
@@ -0,0 +1,20 @@
+package com.ttuleja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Tomal on 2017-06-14.
+ */
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class Main {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Main.class, args);
+    }
+
+}
